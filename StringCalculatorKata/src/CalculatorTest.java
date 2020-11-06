f335ea5a3461378dc2ba93ac5c869fb10735b692
ganes <ganes@LAPTOP-UD39T6RS>
2020-11-06 23:27:34 +0530

    Method 3 Pass and Refactoring

--- StringCalculatorKata/src/CalculatorTest.java
+++ StringCalculatorKata/src/CalculatorTest.java
@@ -1,25 +1,32 @@
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
 
+
+
 public class CalculatorTest {
 
+	private Calculator calculator;
+	
+	@BeforeTest
+	public void init() {
+		calculator = new Calculator();
+	}
+	
 	@Test
 	public void shouldReturnZeroOnEmptyString() { 				// Method 1
-		Calculator calculator = new Calculator();
 		assertEquals(0,calculator.Add(""));
 	}
 	
 	@Test
 	public void shouldReturnNumberOnNumber() {					//Method 2
-		Calculator calculator = new Calculator();
 		assertEquals(calculator.Add("1"),1);
 	}
 	
 	@Test
 	public void twoNumbersCommaDelimitedReturnSum() {			//Method 3
-		Calculator calculator = new Calculator();
 		assertEquals(calculator.Add("1,2"),3);
 	}
 }
