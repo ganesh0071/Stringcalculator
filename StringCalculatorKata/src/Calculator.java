5ab9742cce1eff7512819546395f41346a6c8670
ganes <ganes@LAPTOP-UD39T6RS>
2020-11-06 19:17:58 +0530

    Method 3 Pass

--- StringCalculatorKata/src/Calculator.java
+++ StringCalculatorKata/src/Calculator.java
@@ -2,10 +2,17 @@
 public class Calculator {
 
 	public int Add(String input) {
+		String[] numbers = input.split(",");
 		if(isEmpty(input)) {
 			return 0;	
 		}
-		return stringToInt(input);
+		else if (numbers.length==1) {
+			return Integer.parseInt(input);
+		}
+		else  {
+			return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+		}
+	
 	}
 	
 	private boolean isEmpty(String input) {
